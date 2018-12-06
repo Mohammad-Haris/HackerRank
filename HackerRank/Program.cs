27c18f2345b0e0d581413351311d24c2b1c6ff75
Agha.Mohammad.Haris <Agha.Mohammad.Haris@gmail.com>
2018-12-06 16:43:46 +0500

    Solved Bead Ornaments

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
 using HackerRank.Source.Algorithms.Easy.BitManipulation;
 using HackerRank.Source.Algorithms.Easy.GameTheory;
 using HackerRank.Source.Algorithms.Easy.Greedy;
@@ -9,12 +10,12 @@ using HackerRank.Source.Algorithms.Easy.Search;
 using HackerRank.Source.Algorithms.Hard.DynamicProgramming;
 using HackerRank.Source.Algorithms.Hard.Implementation;
 using HackerRank.Source.Algorithms.Hard.GraphTheory;
-
+using HackerRank.Source.Algorithms.Advanced.GraphTheory;
 
 namespace HackerRank
-{    
+{
     class Program
-    {
+    {       
         static void Main (string[] args)
         {
         }
---
+++ HackerRank/Source/Algorithms/Advanced/GraphTheory/BeadOrnaments.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace HackerRank.Source.Algorithms.Advanced.GraphTheory
+{
+    /// <summary>
+    /// Bead Ornaments
+    /// Problem solving: Algorithms
+    /// Difficulty: Advanced
+    /// SubDomain: Graph Theory
+    /// https://www.hackerrank.com/challenges/beadornaments/problem
+    /// </summary>
+
+    class BeadOrnaments
+    {
+        static int Implement (int[] colors)
+        {
+            BigInteger ornaments = 1;
+            int length = colors.Length;
+
+            if (length > 1)
+            {
+                foreach (int color in colors)
+                {
+                    ornaments *= color;
+                    if (color <= 2) continue;
+                    ornaments *= BigInteger.Pow(color, color - 2);
+                    ornaments %= 1000000007;
+                }
+            }
+
+            else
+            {
+                foreach (int color in colors)
+                {
+                    if (color <= 2) continue;
+                    ornaments *= BigInteger.Pow(color, color - 2);
+                    ornaments %= 1000000007;
+                }
+            }
+
+            if (length > 2) ornaments *= BigInteger.Pow(colors.Sum(), length - 2);
+
+            return (int)(ornaments % 1000000007);
+        }
+
+        /// <summary>
+        /// Input format:
+        /// First line: number of test cases
+        /// Second line: number of colors
+        /// Third line: space seperated number of beads per color
+        /// </summary>
+        
+        static internal void Apply ()
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int bCount = Convert.ToInt32(Console.ReadLine());
+                int[] colors = Array.ConvertAll(Console.ReadLine().Split(' '), bTemp => Convert.ToInt32(bTemp));
+                int result = Implement(colors);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
