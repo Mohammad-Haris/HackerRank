0851737bd8c5b44253f926720ece830c8fa3358c
Agha.Mohammad.Haris <Agha.Mohammad.Haris@gmail.com>
2018-12-31 09:23:33 +0500

    And some more

---
+++ HackerRank/Source/Algorithms/Easy/Implementation/EqualizeTheArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Source.Algorithms.Easy.Implementation
+{
+    class EqualizeTheArray
+    {
+        /// <summary>
+        /// Equalize The Array
+        /// Problem solving: Algorithms
+        /// Difficulty: Easy
+        /// SubDomain: Implementation
+        /// https://www.hackerrank.com/challenges/equality-in-a-array/problem
+        /// </summary>
+
+        static int Implement (int[] arr)
+        {
+            int prevL = arr.Length, currL;
+
+            var dictionary = arr.GroupBy(p => p).ToDictionary(g => g.Key, g => g.Count());
+            int max = dictionary.FirstOrDefault(x => x.Value == dictionary.Values.Max()).Key;
+
+            arr = arr.Where(x => x == max).ToArray();
+            currL = arr.Length;
+
+            return prevL - currL;
+        }
+
+        /// <summary>
+        /// Input format:
+        /// First line: Size of integer array
+        /// Second line: Integer array
+        /// </summary>
+        
+        internal static void Apply ()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int result = Implement(arr);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HackerRank/Source/Algorithms/Easy/Implementation/JumpingOnTheClouds.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HackerRank.Source.Algorithms.Easy.Implementation
+{
+    /// <summary>
+    /// Jumping On The Clouds
+    /// Problem solving: Algorithms
+    /// Difficulty: Easy
+    /// SubDomain: Implementation
+    /// https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+    /// </summary>
+
+    class JumpingOnTheClouds
+    {
+        static int Implement (int[] clouds)
+        {
+            int zeroes = 0, jumps = 0;
+
+            for (int loop = 0; loop < clouds.Length; loop++)
+            {
+                if (clouds[loop] == 0)
+                {
+                    zeroes++;
+                }
+
+                else
+                {
+                    jumps += zeroes / 2 + 1;
+                    zeroes = 0;
+                }
+            }
+
+            jumps += zeroes / 2;
+
+            return jumps;
+        }
+
+        /// <summary>
+        /// Input format:
+        /// First line: Size of integer array
+        /// Second line: Integer array
+        /// </summary>
+        
+        internal static void Apply ()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+            ;
+            int result = Implement(c);
+
+            Console.WriteLine(result);
+        }
+    }
+}
